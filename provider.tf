b50bc8676f48c24129adb4091735d848fe215c2f
Roberto Chiabra <rchiabra@pe.ibm.com>
2020-05-06 08:47:57 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "ibm" {
 #  generation            = 2
   region                = var.ibmcloud_region
-  ibmcloud_api_key      = var.ibmcloud_api_key
+#  ibmcloud_api_key      = var.ibmcloud_api_key
 #  ibmcloud_api_key = "<api_key>"
 #  iaas_classic_username = "<classic_username>"
 #  iaas_classic_api_key = "<classic_api_key>"
